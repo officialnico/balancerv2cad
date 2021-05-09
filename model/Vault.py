62a143609dd57c68526953daee7fa9b246fa19f0
nico <24nico@gmail.com>
2021-05-09 19:57:24 -0400

    Update Vault.py

--- model/Vault.py
+++ model/Vault.py
@@ -26,7 +26,7 @@ class Vault:
     def getPoolTokenInfo(self, cash: Decimal, managed: Decimal, blockNumber: int, assetManager: int):
         ...
 
-    def getPoolTokens(self, tokens: list, balances: list[Decimal], lastChangeBlock: int)
+    def getPoolTokens(self, tokens: list, balances: list[Decimal], lastChangeBlock: int):
         ...
 
     def joinPool(self, poolId: int, sender_id: int, recipent_id: int):
