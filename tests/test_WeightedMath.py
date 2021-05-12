ffb4787b4eb68bb33a1826e61ba9dd27e2770a3f
Martin Jaczynski <martin@Martins-MacBook-Pro.local>
2021-05-12 16:31:31 -0400

    testing weightedmath

--- tests/test_WeightedMath.py
+++ tests/test_WeightedMath.py
@@ -13,7 +13,7 @@ class TestWeightedMath:
 	def test_calc_out_given_in(weightedmath_test):
 		balance_in = Decimal(100)
 		weight_in = Decimal(0.00769)
-		balance_out =  Decimal(100)
+		balance_out = Decimal(100)
 		weight_out = Decimal(1)
 		amount_out = Decimal(15)
 		result = WeightedMath.calc_out_given_in(balance_in, weight_in, balance_out, weight_out, amount_out)
@@ -29,21 +29,47 @@ class TestWeightedMath:
 		assert isinstance(result, Decimal)
 
 
-	def test_calcTokenInGivenExactBptOut(weightedmath_test):
+	def test_calc_token_in_given_exact_bpt_out(weightedmath_test):
 		balance = Decimal(1)
-		normalizedWeight = Decimal(10)
-		bptAmountOut = Decimal(10)
-		bptTotalSupply = Decimal(10)
+		normalized_weight = Decimal(10)
+		bpt_amount_out = Decimal(10)
+		bpt_total_supply = Decimal(10)
 		swap_fee = Decimal(10)
-		result = WeightedMath.calcTokenInGivenExactBptOut(balance, normalizedWeight, bptAmountOut, bptTotalSupply, swap_fee)
+		result = WeightedMath.calc_token_in_given_exact_bpt_out(balance, normalized_weight, bpt_amount_out, bpt_total_supply, swap_fee)
 		assert isinstance(result, Decimal)
 
-	def test_calcBptInGivenExactTokensOut(weightedmath_test):
+	def test_calc_bpt_in_given_exact_tokens_out(weightedmath_test):
 		balances = [Decimal(10), Decimal(12), Decimal(14)]
 		normalized_weights = [Decimal(10), Decimal(12), Decimal(14)]
-		bptAmountOut = [Decimal(10), Decimal(12), Decimal(14)]
-		bptTotalSupply = Decimal(1)
-		swapFee = Decimal(1)
-		result = WeightedMath.calcBptInGivenExactTokensOut(balances, normalized_weights, bptAmountOut, bptTotalSupply, swapFee)
+		bpt_amount_out = [Decimal(10), Decimal(12), Decimal(14)]
+		bpt_total_supply = Decimal(1)
+		swap_fee = Decimal(1)
+		result = WeightedMath.calc_bpt_in_given_exact_tokens_out(balances, normalized_weights, bpt_amount_out, bpt_total_supply, swap_fee)
+		assert isinstance(result, Decimal)
+
+	def test_calc_token_out_given_exact_bpt_in(weightedmath_test):
+		balance = Decimal(10)
+		normalized_weight = Decimal(10)
+		bpt_amount_in = Decimal(2)
+		bpt_total_supply = Decimal(10)
+		swap_fee = Decimal(0)
+		result = WeightedMath.calc_token_out_given_exact_bpt_in(balance, normalized_weight, bpt_amount_in, bpt_total_supply, swap_fee)
+		assert isinstance(result, Decimal)
+
+	def test_calc_tokens_out_given_exact_bpt_in(weightedmath_test):
+		balances = [Decimal(10), Decimal(12), Decimal(14)]
+		bpt_amount_in = Decimal(2)
+		total_bpt = Decimal(1)
+		result  = WeightedMath.calc_token_out_given_exact_bpt_in(balances, bpt_amount_in, total_bpt)
+		assert isinstance(result, Decimal)
+		# TODO this is pretty wrong according to the .sol file
+
+	def test_calc_due_token_protocol_swap_fee_amount(weightedmath_test):
+		balance = Decimal(10)
+		normalized_weight = Decimal(1)
+		previous_invariant = Decimal(2)
+		current_invariant = Decimal(4)
+		protocol_swap_fee_percentage = Decimal(1)
+		result = WeightedMath.calc_due_token_protocol_swap_fee_amount(balance, normalized_weight, previous_invariant, current_invariant, protocol_swap_fee_percentage)
 		assert isinstance(result, Decimal)
 
