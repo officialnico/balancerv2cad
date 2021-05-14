32742621c0e21fb371f2c5a50439a3072150dbe0
nico <24nico@gmail.com>
2021-05-13 20:46:45 -0400

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -2,6 +2,8 @@ from model.pools.stable.StableMath import StableMath
 from model.pools.weighted.WeightedMath import WeightedMath
 from model.pools.BalancerPoolToken import BalancerPoolToken
 from model.pools.BasePool import BasePool
+from model.vault.Vault import Vault
+
 
 import pytest
 
@@ -16,9 +18,12 @@ def weightedmath_test() -> None:
 
 @pytest.fixture()
 def basepool_test() -> None:
-    yield BasePool
+    vault = Vault()
+    basepool = BasePool(vault, 'foo', 'FOO', {'btc','eth':200},Decimal(0.2),0,0)
+    yield basepool
 
 @pytest.fixture()
 def balancer_pool_token_test() -> None:
     yield BalancerPoolToken
+
 
