5d1d30ab343125d06b8b700ab8a1ced6122a6afe
Martin Jaczynski <martin@Martins-MacBook-Pro.local>
2021-05-13 21:29:30 -0400

    starting BasePool testing

--- tests/test_BasePool.py
+++ tests/test_BasePool.py
@@ -1,3 +1,37 @@
 from typing import List
 from model.pools.BasePool import BasePool
 from decimal import Decimal
+import unittest
+
+
+class TestBasePool(unittest.TestCase):
+
+	def test_get_vault(basepool_test):
+		b = BasePool()
+		b.vault = BasePool.vault
+		result = b.get_vault()
+		assert isinstance(result, object)
+
+	def test_get_pool_id(basepool_test):
+		... #TODO
+
+	def test_get_total_tokens(basepool_test):
+		a, b = BasePool, BasePool
+		a.tokens = []
+		b.tokens = ['eth', 'btc']
+		assert a.get_total_tokens() == 0
+		assert b.get_total_tokens() == 2
+
+	def test_get_swap_fee_percentage(basepool_test):
+		b = BasePool
+		b.swap_fee_percentage = 20
+		assert b.get_swap_fee_percentage() == 20
+
+	def test_set_swap_fee_percentage(selfbasepool_test):
+		b = BasePool
+		b.
+
+
+
+
+
