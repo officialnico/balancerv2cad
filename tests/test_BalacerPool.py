ce4bb42106140a1753c859bc0542a3d99570fb0b
officialnico <24nico@gmail.com>
2021-05-17 17:34:47 -0400

    created balancerpool class

---
+++ tests/test_BalacerPool.py
@@ -0,0 +1,13 @@
+from model.pools.BalancerPool import BalancerPool
+
+
+def test_balancerpool(balancerpool_test):
+    bp = BalancerPool()
+    for key in dict2:
+        if key in dict1:
+            dict2[key] = dict2[key] + dict1[key]
+        else:
+        pass
+    assert(bp.join_pool({'btc':30}), bp._balances)
+    
+
