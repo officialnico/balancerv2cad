7a7cb79700e27622b5d50b60043481ea4d60855b
officialnico <24nico@gmail.com>
2021-06-29 09:49:04 -0400

    added balance getting and fees

--- src/BalancerV2cad/StablePool.py
+++ src/BalancerV2cad/StablePool.py
@@ -1,60 +1,46 @@
 from decimal import *
 from typing import List
 from balancerv2cad.StableMath import StableMath
+from balancerv2cad.WeightedPool import WeightedPool
 from balancerv2cad.BalancerConstants import *
 
 
 class StablePool(StableMath):
 
     def __init__(self, initial_pool_supply: Decimal = INIT_POOL_SUPPLY):
-        self._swap_fee = MIN_FEE
-        self._pool_token_supply = initial_pool_supply
-        self.factory_fees = Decimal('0')
-        self._balances = {}
+        self._wp = WeightedPool(INIT_POOL_SUPPLY)
 
     def swap(self, token_in: str, token_out: str, amount: Decimal, given_in: bool = True):
-        if(isinstance(amount,int) or isinstance(amount,float)):
-            amount = Decimal(amount)
-        elif(not isinstance(amount, Decimal)):
-            raise Exception("INCORRECT_TYPE")
-        factory_fee = amount*self._swap_fee
-        swap_amount = amount - factory_fee
-        self.factory_fees += factory_fee
-        balances = [self._balances[token_in], self._balances[token_out]]
-
-        if(given_in): amount_out = StableMath.calcOutGivenIn(AMPLIFICATION_PARAMETER, balances, 0, 1, swap_amount)
-        else: amount_out = StableMath.calcInGivenOut(AMPLIFICATION_PARAMETER, balances, 0, 1, swap_amount)
-
-        self._balances[token_out] -= amount_out
-        self._balances[token_in] += swap_amount
-        return amount_out
+        return self._wp.swap(token_in, token_out, amount, given_in)
 
     def join_pool(self, balances: dict):
-        if len(balances) != 2:
+        if(len(balances) != 2 or len(self._wp._balances)==2 and self._wp._balances.keys()!=balances.keys()):
             raise Exception("50/50 2-token pool only")
+        weights = {}
         for key in balances:
-            if key in self._balances:
-                self._balances[key] += balances[key]
-            else:
-                self._balances.update({key:balances[key]})
+            weights.update({key:0.5})
+        self._wp.join_pool(balances,weights)
 
     def get_amplification_parameter(self):
-        return self.AMPLIFICATION_PARAMETER
+        return self._wp.AMPLIFICATION_PARAMETER
 
     def _get_total_tokens(self):
-        return len(self._balancesa)
+        return len(self._wp._balances)
 
     def exit_pool(self, balances: dict):
-        bals = self._balances - balances
-        for key in bals:
-            if(bals[key]<0): bals[key] = 0
-        self._balances = bals
+        self._wp.exit_pool(balances)
+
     def set_swap_fee(self, amount: Decimal):
-        self._swap_fee = amount
+        self._wp.set_swap_fee(amount)
 
     def _mint_pool_share(self, amount: Decimal):
-        self._pool_token_supply += amount
+        self._wp._pool_token_supply += amount
 
     def _burn_pool_share(self, amount: Decimal):
-        self._pool_token_supply -= amount
+        self._wp._pool_token_supply -= amount
 
+    def get_balances(self):
+        return self._wp._balances
+
+    def get_factory_fees(self):
+        return self._wp.factory_fees
--- tests/unit-test/test_kickstart.py
+++ tests/unit-test/test_kickstart.py
@@ -7,20 +7,6 @@ Showcasing two different ways to properly test your code
 import balancerv2cad as ks
 
 
-def test_version():
-    """
-    testing version from importing it
-    """
-    assert ks.__version__ == '0.1.0'
-
-
-def test_version_using_fixture(version_test):
-    """
-    testing version from passing the module
-    in as a fixture
-    """
-    assert version_test.__version__ == '0.1.0'
-
 
 def test_pkgname_using_fixture(version_test):
     """
